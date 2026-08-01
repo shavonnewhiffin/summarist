a339b151b170b3b97dd49fd56142865c732b0f9f
shavonnewhiffin <shavonnewhiffin@gmail.com>
2026-07-31 17:46:39 -0700

    fix: added Landing component

--- summarist/app/page.js
+++ summarist/app/page.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Landing from './components/Landing'
 
 export default function Home() {
   return (
     <><Navbar /><div>
-      Hello world
+      <Landing />
     </div></>
   );
 }
